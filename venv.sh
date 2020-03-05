ff9f5ddab33cdc4fc2c4ca782ae4db153de319e7
yhlleo <yahui.cvrs@gmail.com>
2020-03-04 22:46:19 -0500

    update install setuptools

--- venv.sh
+++ venv.sh
@@ -2,4 +2,4 @@ rm -rf $HOME/.venv/pointnet2
 virtualenv $HOME/.venv/pointnet2
 source $HOME/.venv/pointnet2/bin/activate
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U
-pip install setuptools -U
+pip install "setuptools<39" -U
