093d2d8c5aceb5eb59b71be9cb93f692db385322
zhaojinjie <zhaojinjie96@gmail.com>
2020-03-07 23:23:52 +0800

    use pointnet2 in server.py

--- part_seg/server.py
+++ part_seg/server.py
@@ -1,18 +1,23 @@
 from __future__ import print_function
+
+from concurrent import futures
+from functools import partial
+import grpc
+
+import evaluate_part
 import pointnet2_pb2_grpc
 import converter
-from concurrent import futures
-import grpc
 
 
 class Server(pointnet2_pb2_grpc.PointServicer):
     def __init__(self):
-        pass
+        s, o = evaluate_part.evaluate()
+        self.eval_one = partial(evaluate_part.eval_one, s, o)
 
     def Segment(self, request_iterator, context):
         for request in request_iterator:
             data = converter.point_request_to_ndarray(request)
-            reply_data = data[:, -1]
+            reply_data = self.eval_one(data)
             reply = converter.ndarray_to_point_reply(reply_data)
             yield reply
 
