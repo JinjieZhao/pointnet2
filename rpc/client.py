faef554fb8020fa4e85c63a48175c0ccd663ffb8
zhaojinjie <zhaojinjie96@gmail.com>
2020-03-08 10:48:51 +0800

    use list_data() in grpc

--- rpc/client.py
+++ rpc/client.py
@@ -2,23 +2,15 @@ from __future__ import print_function
 
 import time
 
-import os
-import numpy as np
 import grpc
 
 import pointnet2_pb2_grpc
 import converter
 import part_dataset_all_normal
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ROOT_DIR = os.path.dirname(BASE_DIR)
-DATA_PATH = os.path.join(ROOT_DIR, 'data', 'shapenetcore_partanno_segmentation_benchmark_v0_normal')
-TEST_DATASET = part_dataset_all_normal.PartNormalDataset(root=DATA_PATH, npoints=2048, classification=False,
-                                                         split='test')
 
-
-def get_data(num):
-    for data in (TEST_DATASET.get_data(i) for i in np.random.choice(1000, num)):
+def get_data(dataset):
+    for data in dataset:
         request = converter.ndarray_to_point_request(data)
         yield request
 
@@ -27,10 +19,11 @@ def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = pointnet2_pb2_grpc.PointStub(channel)
         cnt = 100
+        dataset = part_dataset_all_normal.list_data(cnt)
 
+        data_list = get_data(dataset)
         start = time.time()
-        replies = stub.Segment(get_data(cnt))
-        for reply in replies:
+        for reply in stub.Segment(data_list):
             data = converter.point_reply_to_ndarray(reply)
         end = time.time()
         print(end - start, 's')
