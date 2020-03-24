d45fb8972ee0d4dcb9b05cfbd9249cab85a5d5a9
zhaojinjie <zhaojinjie96@gmail.com>
2020-03-24 12:17:34 +0800

    update venv to use python3

--- venv.sh
+++ venv.sh
@@ -1,5 +1,5 @@
 rm -rf $HOME/.venv/pointnet2
-virtualenv $HOME/.venv/pointnet2
+/home/zhaojinjie/.local/bin/python3.6 -m venv $HOME/.venv/pointnet2
 source $HOME/.venv/pointnet2/bin/activate
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U
-pip install "setuptools<39" -U
+pip install setuptools -U
